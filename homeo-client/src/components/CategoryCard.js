540c9090d0b0149d2e84e468145454b65745df89
andreas <aoysseos91@gmail.com>
2020-04-06 20:57:48 +0100

    ADD image lazy load

--- homeo-client/src/components/CategoryCard.js
+++ homeo-client/src/components/CategoryCard.js
@@ -1,63 +1,56 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import _get from 'lodash/get'
-import { makeStyles } from '@material-ui/core/styles'
-import { Card, CardMedia, Typography } from '@material-ui/core'
-
 import styled from 'styled-components'
+import _get from 'lodash/get'
+import { Typography } from '@material-ui/core'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		position: 'relative'
-	},
-	media: {
-		height: 0,
-		paddingTop: '56.25%' // 16:9
-	}
-}))
-
-const CardStyledMedia = styled(CardMedia)`
-  &:after {
-    content: '';
-    width: 100%;
-    height: 100%;
-    display: block;
-    background-color: #000;
-    z-index: 1;
-    position: absolute;
-    top: 0;
-    left: 0;
-    opacity: .2;
-  }
-`
-
-const CardTitle = styled.div`
-	color: ${({ theme }) => theme.secondary};
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	padding: 0 .5rem;
-	z-index: 2;
+const Card = styled(Link)`
+	height: 13rem;
+	width: 33.3%;
+	position: relative;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 9px;
+	overflow: hidden;
 	> h3 {
+		z-index: 2;
+		color: ${({ theme }) => theme.secondary};
 		font-weight: 700;
+	}
+	> span {
+		width: 100%;
+		position: absolute;
+		> img {
+			width: 100%;
+			height: auto;
+		}
+		
+		&:after {
+			content: '';
+			width: 100%;
+			height: 100%;
+			display: block;
+			background-color: #000;
+			z-index: 1;
+			position: absolute;
+			top: 0;
+			left: 0;
+			opacity: .2;
+		}
 	}
 `
 
 const ClassCard = ({ id, slug, name, image }) => {
-	const classes = useStyles()
-	console.log({ image })
 	return (
-		<Link to={`categories/${id}`}>
-			<Card className={classes.root}>
-				<CardStyledMedia className={classes.media} image={_get(image, 'url')} title={name} />
-				<CardTitle>
-					<Typography variant="h3" component="h3">
-						{name}
-					</Typography>
-				</CardTitle>
-			</Card>
-		</Link>
+		<Card to={`categories/${id}`}>
+			<LazyLoadImage alt={name} effect="blur" src={_get(image, 'url')} />
+			<Typography variant="h3" component="h3">
+				{name}
+			</Typography>
+		</Card>
 	)
 }
 
